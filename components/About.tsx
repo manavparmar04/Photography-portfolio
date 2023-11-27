d086214e3e425ae5629b488f20c9230cbab719ef
Manav Parmar <manavparmar@Manavs-MacBook-Pro.local>
2023-11-26 19:42:05 -0500

    improving image quality testing

--- components/About.tsx
+++ components/About.tsx
@@ -1,4 +1,3 @@
-import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 
@@ -23,39 +22,29 @@ export default function About({}: Props) {
   }, [currentImage]);
 
   return (
-    <div className="h-screen flex flex-col relative text-center md:text-center md:flex-row max-w-7xl px-1 justify-center mx-auto items-center ">
+    <div className="h-screen flex flex-col relative text-right md:text-center md:flex-row max-w-7xl px-1 justify-center mx-auto items-center ">
       <h3 className="  sm:absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl pb-3">
         {" "}
-        {/* Change to justify-center to center about */}
         About
       </h3>
 
-      <motion.div
-        key={currentImage}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 3 }}
-      >
+      <div>
         <Image
           src={currentImage}
           alt="Current Image"
           width={200} // replace with your desired width
           height={300} // replace with your desired height
-          className="-mb-1 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-[3500px] md:h-[500px] xl:w-[4500px] xl:h-[600px]"
+          unoptimized={true}
+          className="-mb-1 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-[3500px] md:h-[500px] xl:w-[4500px] xl:h-[500px]"
         />
-      </motion.div>
+      </div>
 
       <div className="px-0 md:px-10">
         <h4 className="text-4xl font-semibold">
           Little background about <span className="text-[#F7AB0A]"> Me</span>{" "}
         </h4>
 
-        <motion.p
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1.5 }}
-          className="text-base p-4 space-y-10"
-        >
+        <p className="text-base p-4 space-y-10">
           Hello, My name is Manav Parmar, currently, a second-year Computer
           Science student at Toronto Metropolitan University. Since elementary
           school Photography has been a passion of mine, Over the years, I’ve
@@ -68,7 +57,7 @@ export default function About({}: Props) {
           my social media platforms or send me an email. You’ll find all the
           necessary details on the contact page below. I look forward to hearing
           from you!
-        </motion.p>
+        </p>
       </div>
     </div>
   );
